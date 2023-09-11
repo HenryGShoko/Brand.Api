c64454c934692a9667c2abfedb9df2ccda029a00
Henry G Shoko <gshokx91@gmail.com>
2023-09-12 01:26:05 +0200

    Brands controller

---
+++ Controllers/BrandsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brand.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+    }
+}
