2c12bb7b03c12ae290ab7fd6777459ede538b8f1
Henry G Shoko <gshokx91@gmail.com>
2023-09-12 00:32:03 +0200

    Domain models

---
+++ Models/Domain/Brands.cs
@@ -0,0 +1,10 @@
+﻿namespace Brand.Api.Models.Domain
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsPublished { get; set; }
+    }
+
+}
