d47b68222707a721ea67949c0cadb891d2853f22
Henry G Shoko <gshokx91@gmail.com>
2023-09-12 01:37:56 +0200

    brands controller

--- Controllers/BrandsController.cs
+++ Controllers/BrandsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Brand.Api.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 
 namespace Brand.Api.Controllers
 {
@@ -7,5 +10,35 @@ namespace Brand.Api.Controllers
     [ApiController]
     public class BrandsController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public BrandsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetBrands(string sortOrder, bool? isPublished)
+        {
+            var brands = _context.Brands.AsQueryable();
+
+            if (isPublished.HasValue)
+            {
+                brands = brands.Where(b => b.IsPublished == isPublished.Value);
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    brands = brands.OrderByDescending(b => b.Name);
+                    break;
+                default:
+                    brands = brands.OrderBy(b => b.Name);
+                    break;
+            }
+
+            return Ok(brands.ToList());
+        }
     }
+
 }
